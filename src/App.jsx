45ca778f0f752a443c80f7b876216b7f3ffb4df2
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-04-23 23:02:38 +0500

    add playlist page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Like from "./components/Like/like";
 import Playlist from "./components/Playlist/playlist";
 // import NotFound from "./components/NotFound/notFound";
 import Right from "./components/fixed/right";
-import Left from "./components/fixed/left";
+// import Left from "./components/fixed/left";
 
 const App = () => {
   return (
     <Router>
       <div className="body">
-        <Left />
+        {/* <Left /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/like" element={<Like />} />
--- src/components/Playlist/playlist.jsx
+++ src/components/Playlist/playlist.jsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { All } from "../links";
 import { getPlaylists, getToken } from "../fetchData";
+import "./playlist.scss";
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoIosArrowDown,
+} from "react-icons/io";
+import { FaCirclePlay } from "react-icons/fa6";
+import { FcLike } from "react-icons/fc";
+import { CiSaveDown1 } from "react-icons/ci";
+import { BsThreeDots } from "react-icons/bs";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdOutlineWatchLater } from "react-icons/md";
 
 const Details = () => {
   const { id } = useParams();
@@ -10,6 +22,7 @@ const Details = () => {
   const tokenURl = "https://accounts.spotify.com/api/token";
 
   const [data, setData] = useState(null);
+  const [album, setAlbum] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,6 +34,7 @@ const Details = () => {
         );
         console.log(album);
         setData(playlists?.playlists.items);
+        setAlbum(album);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -30,18 +44,81 @@ const Details = () => {
 
   return (
     <div className="playlist-content">
-      <h1 style={{ color: "red" }}>
+      <div>
         {data?.map((el, i) => {
-          console.log(el);
+          // console.log(el);
           if (el?.id === id) {
             return (
               <div key={i}>
-                <h1>{el?.name}</h1>
+                <div className="playlist-top">
+                  <div className="arrows">
+                    <IoIosArrowBack className="IoIosArrowBack" />
+                    <IoIosArrowForward className="IoIosArrowForward" />
+                  </div>
+                  <div className="playlist_top-texts">
+                    <img src={el?.images[0].url} alt={el?.name} />
+                    <div className="texts">
+                      <h6>PUBLIC PLAYLIST</h6>
+                      <h1>{el?.name}</h1>
+                      <p>
+                        Julia Wolf, ayokay, Khalid <span>and more</span>
+                      </p>
+                      <p>
+                        <span>ade for </span>davedirect3
+                        <span>34 songs, 2hr 01 min</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="padding">
+                  <div className="actions">
+                    <div className="buttons">
+                      <FaCirclePlay className="FaCirclePlay" />
+                      <FcLike className="FcLike" />
+                      <CiSaveDown1 className="CiSaveDown1" />
+                      <BsThreeDots className="BsThreeDots" />
+                    </div>
+                    <div className="search">
+                      <IoSearchOutline className="IoSearchOutline" />
+                      <p>Custom order</p>
+                      <IoIosArrowDown className="IoIosArrowDown" />
+                      <p></p>
+                    </div>
+                  </div>
+                  <table className="table table-dark table-hover ">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">TITLE</th>
+                        <th scope="col">ALBUM</th>
+                        <th scope="col">DATE ADDED</th>
+                        <th scope="col">
+                          <MdOutlineWatchLater />
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {album.items.map((items) => (
+                        <div>
+                          {/* {console.log(items)} */}
+                          <audio src={items.track.preview_url}></audio>
+                        </div>
+                      ))}
+                      <tr>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>time</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             );
           }
         })}
-      </h1>
+      </div>
     </div>
   );
 };
